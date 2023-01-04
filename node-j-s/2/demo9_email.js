086620426bb7dfa8330a54566805ef3f218bc3a1
Lore Smith <loresmith114@gmail.com>
2023-01-03 16:14:39 -0800

    Mobile nav bar excercise from w3schools

---
+++ node-j-s/2/demo9_email.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'loresmith114@gmail.com',
+    pass: 'NewGoo21',
+    tls: {
+      rejectUnauthorized: false
+    }
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
